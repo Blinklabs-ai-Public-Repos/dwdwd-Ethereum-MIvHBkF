bc6bb88afd4525d3e1d3043d274f37c2d2132b44
blinklabs-admin <mike@blinklabs.ai>
2024-07-17 15:57:06 +0800

    Update the Peopleee contract with a burn mechanism to allow token holders or the contract owner to destroy tokens, potentially creating deflationary pressure.

--- hardhat/contracts/Peopleee.sol
+++ hardhat/contracts/Peopleee.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Peopleee is ERC20, Ownable {
+contract Peopleee is ERC20, ERC20Burnable, Ownable {
     uint256 private immutable _maxSupply;
 
     constructor(string memory name_, string memory symbol_, uint256 maxSupply_) ERC20(name_, symbol_) {
@@ -14,6 +15,10 @@ contract Peopleee is ERC20, Ownable {
     function mint(address to, uint256 amount) public onlyOwner {
         require(totalSupply() + amount <= _maxSupply, "Exceeds max supply");
         _mint(to, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override onlyOwner {
+        super.burnFrom(account, amount);
     }
 
     function maxSupply() public view returns (uint256) {
